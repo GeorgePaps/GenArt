3d72880104f5d3f0696c3bbcf3ed99d5d0ce1d97
GeorgePaps <georgezpaps@outlook.com.gr>
2023-07-26 08:59:39 +0300

    Updates in Burlap

--- backgrounds/burlap/sketch.js
+++ backgrounds/burlap/sketch.js
@@ -3,7 +3,12 @@ let col_back = [244, 241, 222]; // Eggshell
 let margin = 60;
 
 // parameters for combing
-
+let combWidth = 2;
+let combHeight = 12;
+let combVarR = 25;
+let combVarG = 25;
+let combVarB = 25;
+let combVarA = 20;
 
 function setup() {
 
@@ -20,10 +25,19 @@ function setup() {
   for (let y = 0; y < height; y++){
     for (let x = 0; x < width; x++){
       let index = (x + y*width) * 4;
-      // pixels[index + 0] = 
-      // pixels[index + 1] = 
-      // pixels[index + 2] = y % 255;
-      // pixels[index + 3] = 2
+      if (floor(x/combWidth) % 2 == 0){
+        pixels[index + 0] += cos(y/combHeight*2*PI)*combVarR; 
+        pixels[index + 1] += cos(y/combHeight*2*PI)*combVarG;
+        pixels[index + 2] += cos(y/combHeight*2*PI)*combVarB;
+        pixels[index + 3] += cos(y/combHeight*2*PI)*combVarA;
+      }
+      else {
+        pixels[index + 0] -= cos(y/combHeight*2*PI)*combVarR; 
+        pixels[index + 1] -= cos(y/combHeight*2*PI)*combVarG;
+        pixels[index + 2] -= cos(y/combHeight*2*PI)*combVarB;
+        pixels[index + 3] -= cos(y/combHeight*2*PI)*combVarA;
+      }
+
     }
   }
   updatePixels();
