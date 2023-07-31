1fc0daffbeaf4e8ad0f4ac689d4ae2a5ccb006e7
GeorgePaps <georgezpaps@outlook.com.gr>
2023-07-31 09:16:42 +0300

    Adds some noise to burlap and some final modifications

--- backgrounds/burlap/sketch.js
+++ backgrounds/burlap/sketch.js
@@ -5,10 +5,13 @@ let margin = 60;
 // parameters for combing
 let combWidth = 2;
 let combHeight = 18;
-let combVarR = 10;
-let combVarG = 10;
-let combVarB = 10;
-let combVarA = 80;
+let combVarR = 20;
+let combVarG = 20;
+let combVarB = 20;
+let combVarA = 30;
+
+let noiseSize = 10;
+let noiseFrequency = 100; // the greater the sparser
 
 function setup() {
 
@@ -19,8 +22,8 @@ function setup() {
   fill(col_back);
   noStroke();
   rect(margin, margin, width - 2*margin, height - 2*margin);
-  fill(215,124,105);
-  rect(margin*2, margin*2, width - 4*margin, height - 4*margin);
+  // fill(215,124,105);
+  // rect(margin*2, margin*2, width - 4*margin, height - 4*margin);
 
   // create canvas
   loadPixels();
@@ -28,16 +31,16 @@ function setup() {
     for (let x = 0; x < width; x++){
       let index = (x + y*width) * 4;
       if (floor(x/combWidth) % 2 == 0){
-        pixels[index + 0] += cos(y/combHeight*2*PI)*combVarR; 
-        pixels[index + 1] += cos(y/combHeight*2*PI)*combVarG;
-        pixels[index + 2] += cos(y/combHeight*2*PI)*combVarB;
-        pixels[index + 3] += cos(y/combHeight*2*PI)*combVarA;
+        pixels[index + 0] += (cos(y/combHeight*2*PI)-1)/2*combVarR - bNoise(x,y, noiseSize, noiseFrequency); 
+        pixels[index + 1] += (cos(y/combHeight*2*PI)-1)/2*combVarG - bNoise(x,y, noiseSize, noiseFrequency);
+        pixels[index + 2] += (cos(y/combHeight*2*PI)-1)/2*combVarB - bNoise(x,y, noiseSize, noiseFrequency);
+        pixels[index + 3] += (cos(y/combHeight*2*PI)-1)/2*combVarA - bNoise(x,y, noiseSize, noiseFrequency);
       }
       else {
-        pixels[index + 0] -= cos(y/combHeight*2*PI)*combVarR; 
-        pixels[index + 1] -= cos(y/combHeight*2*PI)*combVarG;
-        pixels[index + 2] -= cos(y/combHeight*2*PI)*combVarB;
-        pixels[index + 3] -= cos(y/combHeight*2*PI)*combVarA;
+        pixels[index + 0] += (cos(y/combHeight*2*PI - PI)-1)/2*combVarR - bNoise(x,y, noiseSize, noiseFrequency); 
+        pixels[index + 1] += (cos(y/combHeight*2*PI - PI)-1)/2*combVarG - bNoise(x,y, noiseSize, noiseFrequency);
+        pixels[index + 2] += (cos(y/combHeight*2*PI - PI)-1)/2*combVarB - bNoise(x,y, noiseSize, noiseFrequency);
+        pixels[index + 3] += (cos(y/combHeight*2*PI - PI)-1)/2*combVarA - bNoise(x,y, noiseSize, noiseFrequency);
       }
 
     }
@@ -45,6 +48,9 @@ function setup() {
   updatePixels();
 }
 
+function bNoise(x,y,size, frequency){
+  return noise(x/frequency,y/frequency)*size
+}
 
 
 
